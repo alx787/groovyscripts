c2b8861814caffab5877cdb82746c33c8d2d1053
alx787 <kaanova@gmail.com>
2019-01-29 13:27:13 +0300

    сменить уровень безопасности задачи

---
+++ change_sec.level.groovy
@@ -0,0 +1,20 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.user.ApplicationUser
+import com.atlassian.jira.issue.IssueFactory
+import com.atlassian.jira.issue.IssueManager
+import com.atlassian.jira.issue.attachment.FileAttachments
+import org.slf4j.LoggerFactory
+
+import com.atlassian.jira.event.type.EventDispatchOption;
+
+
+IssueManager issueManager = ComponentAccessor.getIssueManager()
+ApplicationUser curUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+
+//MutableIssue mutIssue = issue
+
+issue.setSecurityLevelId(10001)
+
+issueManager.updateIssue(curUser, issue, EventDispatchOption.ISSUE_UPDATED, false);
